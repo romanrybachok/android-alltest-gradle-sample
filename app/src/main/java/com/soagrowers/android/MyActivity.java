9eff32ee46446eb8bdce744c579f308838250af0
romanrybachok <roman.rybachok@gmail.com>
2016-06-02 16:21:35 +0300

    test commit

--- app/src/main/java/com/soagrowers/android/MyActivity.java
+++ app/src/main/java/com/soagrowers/android/MyActivity.java
@@ -7,6 +7,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.TextView;
 
 
 public class MyActivity extends ActionBarActivity {
